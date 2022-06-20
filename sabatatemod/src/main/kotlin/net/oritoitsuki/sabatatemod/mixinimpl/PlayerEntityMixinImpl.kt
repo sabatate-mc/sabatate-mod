c2758e689e734ea1bed4f53c018bb2a27960b1aa
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2022-06-20 22:37:44 +0900

    Fix error in v0.2.0

---
+++ sabatatemod/src/main/kotlin/net/oritoitsuki/sabatatemod/mixinimpl/PlayerEntityMixinImpl.kt
@@ -0,0 +1,23 @@
+package net.oritoitsuki.sabatatemod.mixinimpl
+
+import net.minecraft.entity.damage.DamageSource
+import net.minecraft.entity.damage.EntityDamageSource
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.item.ItemStack
+import net.minecraft.world.World
+import net.minecraft.world.explosion.Explosion
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable
+
+object PlayerEntityMixinImpl {
+    fun onEatFood(self: PlayerEntity, world: World, stack: ItemStack, callbackInfoReturnable: CallbackInfoReturnable<ItemStack>) {
+        val source = EntityDamageSource("sabatate_manju", self)
+        self.damage(source, 3.4e38F)
+    }
+
+    fun onDeath(self: PlayerEntity, source: DamageSource, amount: Float, callbackInfoReturnable: CallbackInfoReturnable<Boolean>) {
+        if (source.name != "sabatate_manju") return
+        if (self.world.isClient) return
+        val pos = self.pos
+        self.world.createExplosion(null, pos.x, pos.y + 1.5F, pos.z, 2F, Explosion.DestructionType.BREAK)
+    }
+}
