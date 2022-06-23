6854730f359764d09ee1378fa2c5ddc525fc2a8c
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2022-06-23 12:31:00 +0900

    Add goriyaku to statue

--- sabatatemod/src/main/kotlin/net/oritoitsuki/sabatatemod/block/blocks/SabatateTokenBlock.kt
+++ sabatatemod/src/main/kotlin/net/oritoitsuki/sabatatemod/block/blocks/SabatateTokenBlock.kt
@@ -1,4 +1,4 @@
-package net.oritoitsuki.sabatatemod.block
+package net.oritoitsuki.sabatatemod.block.blocks
 
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.minecraft.block.Block
