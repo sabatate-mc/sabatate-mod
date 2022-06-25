5ba8293d522ca9a05aa180a0f2c394a81fa16e6d
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2022-06-26 06:59:09 +0900

    Refactor item slot

--- sabatatemod/src/main/kotlin/net/oritoitsuki/sabatatemod/screenhandler/SabatateStatueTopScreenHandler.kt
+++ sabatatemod/src/main/kotlin/net/oritoitsuki/sabatatemod/screenhandler/SabatateStatueTopScreenHandler.kt
@@ -7,7 +7,7 @@ import net.minecraft.inventory.SimpleInventory
 import net.minecraft.item.ItemStack
 import net.minecraft.screen.ScreenHandler
 import net.minecraft.screen.slot.Slot
-import net.minecraft.screen.slot.SlotActionType
+import net.oritoitsuki.sabatatemod.screenhandler.slot.SabatateStatueSlot
 
 class SabatateStatueTopScreenHandler(
     syncId: Int,
@@ -23,7 +23,7 @@ class SabatateStatueTopScreenHandler(
         println(inventory is SimpleInventory)
 
         // Our Inventory
-        addSlot(Slot(inventory, 0, 8 + 18 * 4, 20))
+        addSlot(SabatateStatueSlot(inventory, 0, 8 + 18 * 4, 20))
 
         // player inventory
         for (row in 0 until 3) {
@@ -43,87 +43,20 @@ class SabatateStatueTopScreenHandler(
     }
 
     override fun transferSlot(player: PlayerEntity, index: Int): ItemStack {
-        var newStack = ItemStack.EMPTY
         val slot = slots[index]
         if (slot.hasStack()) {
             val originalStack = slot.stack
-            newStack = originalStack.copy()
             if (index < inventory.size()) {
-                if (!insertItem(originalStack, inventory.size(), slots.size, true)) {
-                    return ItemStack.EMPTY
-                }
-            } else if (!insertItem(originalStack, 0, inventory.size(), false)) {
-                return ItemStack.EMPTY
-            }
-
-            if (originalStack.isEmpty) {
-                slot.stack = ItemStack.EMPTY
+                insertItem(originalStack, inventory.size(), slots.size, true)
             } else {
-                slot.markDirty()
+                val statueSlot = slots[0]
+                if (statueSlot.canInsert(originalStack) && statueSlot.stack.count < 1) {
+                    statueSlot.stack = originalStack.split(1)
+                    statueSlot.markDirty()
+                    slot.markDirty()
+                }
             }
         }
-
-        return newStack
-    }
-
-    override fun onSlotClick(slotIndex: Int, button: Int, actionType: SlotActionType, player: PlayerEntity) {
-        if (slotIndex == 0) {
-            // さばたて石像のスタックをクリックしたとき
-            when(actionType) {
-                SlotActionType.PICKUP, SlotActionType.PICKUP_ALL -> {
-                    // ピックアップ（現在のマウスのスタックと交換）の場合
-                    if (cursorStack.registryEntry.key.get().value.namespace == "minecraft" &&
-                        cursorStack.registryEntry.key.get().value.path == "torch") {
-                        // カーソルがトーチの場合、石像のスタックが空だったら、
-                        // カーソルのスタックから一つ減らして、石像のスタックに一つトーチを置く
-                        val stack = getSlot(slotIndex).stack
-                        if (stack.isEmpty) {
-                            val torch = cursorStack.split(1)
-                            inventory.setStack(0, torch)
-                            inventory.markDirty()
-                        }
-                    } else if (cursorStack.isEmpty) {
-                        // カーソルが空の場合、カーソルにトーチを渡して、石像のスタックからトーチを取り除く
-                        cursorStack = inventory.getStack(0)
-                        inventory.clear()
-                        inventory.markDirty()
-                    } else {
-                        // それ以外の場合、操作をキャンセルする
-                        return
-                    }
-                }
-                SlotActionType.QUICK_MOVE -> {
-                    // クイックムーブの場合、実行する
-                    super.onSlotClick(slotIndex, button, actionType, player)
-                    return
-                }
-            }
-        } else if (slotIndex > 0) {
-            // playerのinventoryのスタックをクリックしたとき
-            when (actionType) {
-                SlotActionType.QUICK_MOVE -> {
-                    // クイックムーブの場合、スタックがトーチのときだけ実行
-                    val stack = getSlot(slotIndex).stack
-                    if (stack.registryEntry.key.get().value.namespace == "minecraft" &&
-                        stack.registryEntry.key.get().value.path == "torch") {
-                        // 石像のスタックに一つトーチを置く
-                        val statueStack = inventory.getStack(0)
-                        if (statueStack.isEmpty) {
-                            val torch = stack.split(1)
-                            inventory.setStack(0, torch)
-                            inventory.markDirty()
-                        }
-                    }
-                    return
-                }
-                else -> {
-                    // それ以外のアクションの場合は実行する
-                    super.onSlotClick(slotIndex, button, actionType, player)
-                }
-            }
-        } else {
-            // network用
-            super.onSlotClick(slotIndex, button, actionType, player)
-        }
+        return ItemStack.EMPTY
     }
 }
